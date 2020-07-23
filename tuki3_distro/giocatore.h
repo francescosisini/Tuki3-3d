e58aedbbac0f844f0375e537ce7c03abedeabf20
francescosisini <francescomichelesisini@gamil.com>
2020-07-23 08:26:17 +0200

    Primo commit

---
+++ tuki3_distro/giocatore.h
@@ -0,0 +1,9 @@
+#define DIM 5
+typedef enum {GEMMA,MELANZANA,POZIONE} tipo_oggetto;
+typedef enum {MANGIA,SALTA,PRENDI} azione;
+typedef struct 
+{
+  float nutrienti;
+  float tossine;
+  float energia;
+} stato;
---
+++ tuki3_distro/turno_tuki.c
@@ -0,0 +1,9 @@
+#include "giocatore.h"
+
+
+azione turno_tuki(char oggetto_incontrato[DIM*DIM], stato st_tuki)
+{
+  /*Scrivi qui il tuo codice*/
+
+  return MANGIA;
+}
